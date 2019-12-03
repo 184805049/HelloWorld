4c8073572b556b4f25e95164ff8f653c360765e4
184805049 <54939662+184805049@users.noreply.github.com>
2019-12-03 21:12:54 +0800

    PBL作业

---
+++ PBL(继承)/src/Example.java
@@ -0,0 +1,14 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Movie m1=new Movie("Java",new ChildrensPrice());
+		Movie m2=new Movie("C++",new NewReleasePrice());
+		Movie m3=new Movie("Python",new NewReleasePrice());
+		
+		
+	    System.out.println("+++");	
+	}
+}
